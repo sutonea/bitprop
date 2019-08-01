2273fbe539234a3a42e710cfa3c2942c871a55a5
s.i <stonhair@gmail.com>
2019-08-01 22:24:07 +0900

    Initial commit

---
+++ lib/bitprop.rb
@@ -0,0 +1,6 @@
+require "bitprop/version"
+
+module Bitprop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bitprop_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Bitprop do
+  it "has a version number" do
+    expect(Bitprop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
