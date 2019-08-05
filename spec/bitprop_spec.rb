86b06d98298e4642e6e514e96c4fb2c3a62632c7
s.i <stonhair@gmail.com>
2019-08-05 23:31:47 +0900

    Add test code

--- spec/bitprop_spec.rb
+++ spec/bitprop_spec.rb
@@ -34,6 +34,23 @@ RSpec.describe Bitprop do
     expect(target_object.third).to  be_falsey
   end
 
+  it "can read as hash" do
+    target_object.my_bits = 0b001
+    expect(target_object.all_bit_attr_hash.fetch(:first)).to  be_truthy
+    expect(target_object.all_bit_attr_hash.fetch(:second)).to be_falsey
+    expect(target_object.all_bit_attr_hash.fetch(:third)).to  be_falsey
+
+    target_object.my_bits = 0b100
+    expect(target_object.all_bit_attr_hash.fetch(:first)).to  be_falsey
+    expect(target_object.all_bit_attr_hash.fetch(:second)).to be_falsey
+    expect(target_object.all_bit_attr_hash.fetch(:third)).to  be_truthy
+
+    target_object.my_bits = 0b010
+    expect(target_object.all_bit_attr_hash.fetch(:first)).to  be_falsey
+    expect(target_object.all_bit_attr_hash.fetch(:second)).to be_truthy
+    expect(target_object.all_bit_attr_hash.fetch(:third)).to  be_falsey
+  end
+
   it "can write" do
     target_object.my_bits = 0b000
     target_object.first = true
